5dcf1c1c1309c4369bb7618ccaa30b7b2c1f4f39
Ross Tang Him <ross710@bu.edu>
2013-11-10 23:29:08 -0500

    Version 1.0 Published

--- src/com/rosstard/BUTransit/CellAdapter.java
+++ src/com/rosstard/BUTransit/CellAdapter.java
@@ -44,6 +44,8 @@ public class CellAdapter extends BaseAdapter {
 		if (size == 0) {
 			size = 1;
 			noBus = true;
+		} else {
+			noBus = false;
 		}
 		return size;
 	}
@@ -72,30 +74,30 @@ public class CellAdapter extends BaseAdapter {
 //		mHolder.addressTextView = (TextView)convertView.findViewById(R.id.address_textView);
 //		mHolder.addressTextView.setText(peopleList.get(position).getAddress());
 		if (!noBus) {
-		mHolder.nameTextView = (TextView) convertView.findViewById(R.id.name_textView);
-		mHolder.nameTextView.setText(stops.get(position).getName());
-		mHolder.directionTextView = (TextView)convertView.findViewById(R.id.direction_textView);
-//		
-		if (stops.get(position).isInboundToStuvi()) {
-			mHolder.directionTextView.setText("(to West Campus)");
-			mHolder.nameTextView.setTextColor(Color.RED);
-			mHolder.directionTextView.setTextColor(Color.RED);
+			mHolder.nameTextView = (TextView) convertView.findViewById(R.id.name_textView);
+			mHolder.nameTextView.setText(stops.get(position).getName());
+			mHolder.directionTextView = (TextView)convertView.findViewById(R.id.direction_textView);
+			//		
+			if (stops.get(position).isInboundToStuvi()) {
+				mHolder.directionTextView.setText("(to West Campus)");
+				mHolder.nameTextView.setTextColor(Color.RED);
+				mHolder.directionTextView.setTextColor(Color.RED);
 
-		} else {
-			mHolder.directionTextView.setText("(to East/Med Campus)");
-			mHolder.nameTextView.setTextColor(Color.WHITE);
-			mHolder.directionTextView.setTextColor(Color.WHITE);
-		}
-			
-		mHolder.sizeTextView = (TextView)convertView.findViewById(R.id.size_textView);
-		mHolder.sizeTextView.setText(stops.get(position).getType());
+			} else {
+				mHolder.directionTextView.setText("(to East/Med Campus)");
+				mHolder.nameTextView.setTextColor(Color.WHITE);
+				mHolder.directionTextView.setTextColor(Color.WHITE);
+			}
 
-		mHolder.nextBusTextView = (TextView)convertView.findViewById(R.id.nextBus_textView);
-		mHolder.nextBusTextView.setText("next bus is:");		
-		mHolder.minsTextView = (TextView)convertView.findViewById(R.id.mins_textView);
-		mHolder.minsTextView.setText(Integer.toString(stops.get(position).getMins()));
-		mHolder.minutesAwayTextView = (TextView)convertView.findViewById(R.id.minutesAway_textView);
-		mHolder.minutesAwayTextView.setText("minutes away");
+			mHolder.sizeTextView = (TextView)convertView.findViewById(R.id.size_textView);
+			mHolder.sizeTextView.setText(stops.get(position).getType());
+
+			mHolder.nextBusTextView = (TextView)convertView.findViewById(R.id.nextBus_textView);
+			mHolder.nextBusTextView.setText("next bus is:");		
+			mHolder.minsTextView = (TextView)convertView.findViewById(R.id.mins_textView);
+			mHolder.minsTextView.setText(Integer.toString(stops.get(position).getMins()));
+			mHolder.minutesAwayTextView = (TextView)convertView.findViewById(R.id.minutesAway_textView);
+			mHolder.minutesAwayTextView.setText("minutes away");
 		} else {
 			mHolder.nameTextView = (TextView) convertView.findViewById(R.id.name_textView);
 			mHolder.nameTextView.setText("Buses aren't running right now");
